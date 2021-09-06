da2271ed3df7be57a27e0c044973f0d368ae60c5
Beki <beki@asterone.co.jp>
2021-09-06 11:34:41 +0500

    initial commit

---
+++ assets/js/fetchStockData.js
@@ -0,0 +1,42 @@
+(function ($) {
+    function csv_data(dataPath) {
+        const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
+        request.addEventListener('load', (event) => { // ロードさせ実行
+            const response = event.target.response; // 受け取ったテキストを返す
+            const lines = response.split('\n'); // 改行で区切る
+            const data = lines.map((line) => line.split(',')); // 各行をカンマで区切る
+            const headers = data[0]; // 先頭行をヘッダーとして抽出
+            const rows = data.slice(1); // 先頭行を除いた全行を行として抽出
+
+            create_headers(headers);
+            create_rows(rows);
+        });
+        request.open('GET', dataPath, true); // csvのパスを指定
+        request.send();
+    }
+    csv_data('../assets/stock.csv'); // csvのパス relative in terms of stocklist.html path
+
+})(window.jQuery);
+
+const create_rows = (row_data) => {
+    const tbody = document.querySelector('#stock-table tbody');
+    row_data.forEach((row) => {
+        const tr = document.createElement('tr');
+        row.forEach((data) => {
+            const td = document.createElement('td');
+            td.innerText = data;
+            tr.appendChild(td);
+        });
+        tbody.appendChild(tr);
+    });
+}
+const create_headers = (header_data) => {
+    const thead = document.querySelector('#stock-table thead');
+    const tr = document.createElement('tr');
+    header_data.forEach((header) => {
+        const th = document.createElement('th');
+        th.innerText = header;
+        tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# やろこと
+- [ ] Footer　＝＞　スタイリング、positionをページの一番下によせること（どのページでも） 
+- [ ] TOPぺーじ　＝＞　会社紹介の見え方を変更
+- [ ] 事業内応　＝＞　`.business-main`を`header`との距離を変更、`.item wow fadeIn animated`をテーブルからrefactorする。、footerの変更
+- [ ] 電子部品　＝＞　cardの高さを同じになるようにする、footerの変更
+- [ ] 光学機器事業　＝＞　footerの変更
+- [ ] 会社情報　＝＞　footerの変更、`.business-main`を`header`との距離を変更、`.item wow fadeIn animated`をテーブルからrefactorする。
+- [ ] 会社概要　＝＞　footerの変更、アクセス方法でマップのwidthを１００％か、undrawからillustrationをとってくるか、マップのタイトルをキレイにする
+- [ ] 会社沿革　＝＞　footerの変更、
+- [ ] 在庫情報　＝＞　footerの変更、事業内応と同じくする
+- [ ] 取り扱いメーカー　＝＞　メーカー、住所、電話番号のテーブルを作って、適当な情報を入れる
+- [ ] 在庫一覧　＝＞　footerの変更
+- [ ] お問い合わせ　＝＞　footerの変更、illustrationを入れる、フォームの項目を調整
+- [ ] privacy　＝＞　footerの変更、paddingの調整、項目と項目の間をスペースを大きくする、<h/> と <p/>タグのfont-sizeを調整する。アドレスのところを目立つようにする
+- [ ] site mapを調整、footer調整
